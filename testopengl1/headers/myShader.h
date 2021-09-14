5420d98d23f2f94cca3fb4efb5d02cc8f0f20a09
hellozts <hellozts@gmail.com>
2021-09-14 14:39:17 +0800

    Initial Commit

---
+++ testopengl1/headers/myShader.h
@@ -0,0 +1,12 @@
+//
+//  myShader.h
+//  testopengl1
+//
+//  Created by 郑天省 on 2021/9/14.
+//
+
+#ifndef myShader_h
+#define myShader_h
+
+
+#endif /* myShader_h */
---
+++ testopengl1/headers/myWindow.h
@@ -0,0 +1,48 @@
+//
+//  myWindow.h
+//  testopengl1
+//
+//  Created by 郑天省 on 2021/9/14.
+//
+
+#ifndef myWindow_h
+#define myWindow_h
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void proccessInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+GLFWwindow* createWindow(int width, int height) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow* window = glfwCreateWindow(width, height, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(0);
+    }
+    
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        exit(0);
+    }
+    return window;
+}
+
+#endif /* myWindow_h */
